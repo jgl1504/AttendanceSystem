17dbb66cebfb1755633bd11987762ba3bde8fbdd
Jacques Labuschagne <jgl1504@yahoo.co.uk>
2026-02-04 22:10:56 +0200

    Adding reports

---
+++ WebApp/Shared/Model/DriverOvertimeSummary.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApp.Shared.Model
+{
+    public class DriverOvertimeSummary
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; } = string.Empty;
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public double NormalHours { get; set; }
+        public double WeekdayOvertimeHours { get; set; }
+        public double SundayPublicOvertimeHours { get; set; }
+
+        public double ApprovedOvertimeHours { get; set; }
+        public double UnapprovedOvertimeHours { get; set; }
+    }
+}
---
+++ WebApp/Shared/Model/LeaveSummary.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp.Shared.Model
+{
+    public class LeaveSummary
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; } = string.Empty;
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        // Per leave type for the period
+        public List<LeaveBalanceSummaryDto> LeaveTypes { get; set; } = new();
+    }
+}
